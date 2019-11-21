cc54eb73f1f5e64d580e24581de95905d6d7d9a2
Krishankant <krishankant_rai@rediffmail.com>
2019-11-21 21:01:53 +0000

    Sync LeetCode submission - Defanging an IP Address (javascript)

---
+++ my-folder/problems/defanging_an_ip_address/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    let ans='';
+    for(let i=0; i<address.length; i++){
+        if(address[i]=='.'){
+            ans+='[.]' ;
+        }
+        else
+            {
+                ans+=address[i] ;
+            }
+    }
+    
+    return ans;
+    
+};
