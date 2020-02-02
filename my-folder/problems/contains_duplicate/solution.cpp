9b53c9e056ce9cce7dd393a9164c3036e592e7b4
Krishankant <krishankant_rai@rediffmail.com>
2020-02-02 20:27:43 +0000

    Sync LeetCode submission - Contains Duplicate (cpp)

---
+++ my-folder/problems/contains_duplicate/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> st(nums.begin(), nums.end());
+        if(st.size()==nums.size())
+            return false;
+        return true;
+    }
+};
