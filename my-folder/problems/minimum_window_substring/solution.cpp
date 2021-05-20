319d1cb641efd4d31a48af4dd52552d0ac92a903
Krishankant <krishankant_rai@rediffmail.com>
2021-05-20 09:40:43 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- my-folder/problems/minimum_window_substring/solution.cpp
+++ my-folder/problems/minimum_window_substring/solution.cpp
@@ -1,59 +1,36 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-         string ans ;
-  int start=0,end=0;
-  map<char,int>m;
-  for(int i=0; i<t.length(); i++) ++m[t[i]] ;
-  int ctr=m.size();
-  int minsize=INT_MAX;
-  if(s==t) return s;
-  if(s.length() < t.length())
-  return "" ;
-  while(end<s.length())
-  {
-    if(ctr>0)
-    {
-      if(m.find(s[end]) != m.end())
-      {
-        m[s[end]]-- ;
-        if(m[s[end]]==0)
-        ctr-- ;
+        int sLen = s.size();
+        int tLen = t.size();
+        unordered_map<char,int>mp;
+        for(char ch:t) mp[ch]++ ;
         
-      }
-      end++;
-    }
-
-    while(ctr==0)
-    {
-          if(ctr==0)
-          if((end-start)<minsize)
-            minsize=end-start , ans=s.substr(start, minsize);
-          if(m.find(s[start])==m.end() )
-          {
-            start++;
-
-          }
-          else
-          {
-            ++m[s[start]] ;
-            if(m[s[start]]>0 )
-            {
-
-              ctr++;
+        int ansLen = INT_MAX ; 
+        int ansBeg = -1; 
+        int ctr=mp.size() ;
+        int l=0, r=0 ;
+        
+        while(r<sLen){
+            if(mp.find(s[r])!=mp.end()) --mp[s[r]] ;
+            if(mp.find(s[r])!=mp.end() && mp[s[r]]==0) ctr-- ;
+            r++;
+            while(ctr==0){
+               // cout<<"r= "<<r<<" || "<<" l= "<<l<<endl;
+                if(r-l < ansLen){
+                    ansLen = r-l ; 
+                    ansBeg = l ;
+                }
+                if(mp.find(s[l]) != mp.end()){
+                    mp[s[l]]++ ;
+                    if(mp[s[l]]>0) ctr++ ;
+                }
+                
+                l++ ;
             }
-            start++;
-          }
-
-
-
-
-
-    }
-
-
-  }
-  //cout<<start<<end<<endl;
-  return ans;
+            // if(r==13) cout<<"mp= "<<mp['A']<<", "<<mp['B']<<", "<<mp['C']<<", "<<endl; 
+        }
+        
+        return ansBeg==-1 ? "" : s.substr(ansBeg, ansLen);  
     }
 };
