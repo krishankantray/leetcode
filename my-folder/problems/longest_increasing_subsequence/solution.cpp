c5d35cbecc87f8a57d99ebb3fccc5324459020c4
Krishankant <krishankant_rai@rediffmail.com>
2021-06-24 09:31:50 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (cpp)

---
+++ my-folder/problems/longest_increasing_subsequence/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int ans = INT_MIN ; 
+        int n = nums.size() ; 
+        vector<int>lis(n, 1) ;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[j]<nums[i]){
+                    lis[i]=max(lis[i], lis[j]+1) ;
+                }
+            }
+        }
+       for(int i=0; i<n; i++){
+           ans=max(ans, lis[i]) ;
+       }
+        return ans; 
+    }
+};
