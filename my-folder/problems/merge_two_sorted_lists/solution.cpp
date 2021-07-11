ea13bbb4540e8e422c7cae0a37d47dc0bd0747af
Krishankant <krishankant_rai@rediffmail.com>
2021-07-11 14:44:31 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- my-folder/problems/merge_two_sorted_lists/solution.cpp
+++ my-folder/problems/merge_two_sorted_lists/solution.cpp
@@ -3,34 +3,41 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
-        ListNode* ans = new ListNode(0) ;
-        ListNode* ans_ptr = ans ;
-        
-        if(!(l1 || l2)) return NULL ;
-        
-        while(l1||l2){
-            
-            int val1 = (l1 ? l1->val : INT_MAX );
-            int val2 = (l2 ? l2->val : INT_MAX ) ;
-            
-            if(val1 <= val2){
-                ans_ptr->next = l1 , l1=l1->next ;
+        if(!l1 && !l2) return NULL ;
+        ListNode* newHead = new ListNode(0) ; 
+        ListNode* cpyHead = newHead ; 
+        ListNode* p1 = l1 ;
+        ListNode* p2 = l2 ;
+        while(p1 && p2){
+            if(p1->val < p2->val){
+                newHead->next = p1 ;
+                p1=p1->next ;
             }
-            else
-                ans_ptr->next = l2, l2=l2->next ;
-            
-            ans_ptr = ans_ptr->next ;
-            
+            else{
+                newHead->next = p2;
+                p2=p2->next ;
+            }
+            newHead=newHead->next ;
         }
-        
-        return ans->next ;
-        
+        while(p1){
+            newHead->next = p1 ;
+            p1=p1->next ;
+            newHead = newHead->next ;
+        }
+        while(p2){
+            newHead->next = p2 ;
+            p2=p2->next ;
+            newHead = newHead->next ;
+        }
+        newHead->next = NULL ;
+        return cpyHead->next ; 
     }
 };
