adac8fd9a8f5f95ed74baacead1b5ba471d317d3
Krishankant <krishankant_rai@rediffmail.com>
2021-09-08 05:48:28 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

--- my-folder/problems/longest_consecutive_sequence/solution.cpp
+++ my-folder/problems/longest_consecutive_sequence/solution.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        map<int, int>mp;
-        int category=0 ; 
-        for(int x:nums){
-            if(mp.find(x-1)!=mp.end()){
-                mp[x]=mp[x-1] ; 
+        unordered_map<int, bool>mp;
+        for(int x:nums) mp[x]=true ;
+        int ans=0; 
+        for(int i=0; i<nums.size(); i++){
+            if(mp.find(nums[i]-1) == mp.end()){
+                int ct = nums[i]+1 ; 
+                int cur = 1 ; 
+                while(mp.find(ct) !=mp.end()){
+                    ct++ ;
+                    cur++ ;
+                }
+                ans = max(ans, cur) ; 
             }
-            else if(mp.find(x+1) !=mp.end()){
-                mp[x]=mp[x+1];
-            }
-            else mp[x] = category++ ; 
-        }
-        int ans=0 ;
-        map<int, int>catSum;
-        for(auto mi:mp){
-            auto l = mp.find(mi.first-1) ; 
-            auto r = mp.find(mi.first+1) ; 
-            if(l!=mp.end()) l->second = mi.second ; 
-            if(r!=mp.end()) r->second = mi.second ; 
-            // cout<<"[ "<<mi.first<<":"<<mi.second<<" ],  " ; 
-            ++catSum[mi.second] ;
-            ans=max(ans, catSum[mi.second]) ; 
         }
         return ans ; 
     }
