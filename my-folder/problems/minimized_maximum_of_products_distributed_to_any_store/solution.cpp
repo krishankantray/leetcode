4721fe01e6fd8a65a1dc6bb4f7f597844ad04c11
Krishankant <krishankant_rai@rediffmail.com>
2021-11-09 21:16:17 +0000

    Sync LeetCode submission - Minimized Maximum of Products Distributed to Any Store (cpp)

--- my-folder/problems/minimized_maximum_of_products_distributed_to_any_store/solution.cpp
+++ my-folder/problems/minimized_maximum_of_products_distributed_to_any_store/solution.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int minimizedMaximum(int n, vector<int>& Q) {
-        long long L = 1, R = accumulate(begin(Q), end(Q), 0LL);
-        auto valid = [&](int M) {
-            int ans = 0;
-            for (int n : Q) ans += (n + M - 1) / M; // ceil(n / M)
-            return ans <= n;
-        };
-        while (L <= R) {
-            long long M = (L + R) / 2;
-            if (valid(M)) R = M - 1;
-            else L = M + 1;
+        int L = 1, R = *max_element(Q.begin(), Q.end());
+        int mid=0;
+        while(L<=R){
+            mid=(L+R)/2;
+            int ctr=0;
+            for(int it:Q){
+                if(it<mid){
+                    ctr++;
+                }
+                else{
+                    float numer = (float) it;
+                    float deno = (float) mid; 
+                    ctr+=ceil(numer/deno);
+                    // if(mid==5) cout<<ctr<<endl;
+                }
+            }
+            // cout<<"L="<<L<<" R="<<R<<" ctr="<<ctr<<endl;
+            if(ctr>n) 
+                L=mid+1;
+            else
+                R=mid-1;
         }
-        return L;
+        return L; 
     }
 };
