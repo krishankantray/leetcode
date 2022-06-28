9615d77e41e2bc4435c8d82b732ae8d4f5153e6a
Krishankant <krishankant_rai@rediffmail.com>
2022-06-28 20:53:51 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- my-folder/problems/number_of_islands/solution.cpp
+++ my-folder/problems/number_of_islands/solution.cpp
@@ -1,45 +1,57 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        if(n==0) return 0;
-        int m=grid[0].size();
-        stack<pair<int,int> > dfs;
-        vector<vector<bool> > vis(n, vector<bool>(m,false));
-        int ans=0;
+    void dfs(vector<vector<char> > &grid, map<pair<int,int>,bool> &m, pair<int,int> s)
+    {
+        m[s]=true;
+        int in = grid.size(), jn = grid[0].size() ;
+        int ix = s.first, jx = s.second ;
+        if(ix+1 < in && jx <jn){
+            if(m.find({ix+1,jx})==m.end() && grid[ix+1][jx]=='1')
+            {
+                
+                dfs(grid,m,{ix+1,jx});
+            }
+            
+            }
+        if(ix < in && jx+1 < jn)
+        {
+            if(m.find({ix,jx+1})==m.end() && grid[ix][jx+1]=='1')
+            {
+                dfs(grid,m,{ix,jx+1}) ;
+            }
+        }
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
-                    dfs.push({i,j});
+        if(ix-1 >=0)
+        {
+            if(m.find({ix-1, jx})==m.end() && grid[ix-1][jx]=='1')
+            {
+                dfs(grid,m,{ix-1, jx});
+            }
+        }
+        if(jx-1 >=0)
+        {
+            if(m.find({ix,jx-1})==m.end() && grid[ix][jx-1]=='1')
+            {
+                dfs(grid,m,{ix,jx-1});
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        map<pair<int,int>,bool> m ;
+        int ans=0;
+        for(int i=0; i<grid.size(); i++)
+        {
+            for(int j=0; j<grid[0].size(); j++)
+            {
+                if(m.find({i,j})==m.end() && grid[i][j]=='1')
+                {
                     ans++;
-                    
-                    while(!dfs.empty()){
-                        pair<int,int>cur = dfs.top();
-                        dfs.pop();
-                        int cur_i = cur.first;
-                        int cur_j = cur.second;
-                        vis[cur_i][cur_j]=true;
-                        if(cur_i+1 < n && grid[cur_i+1][cur_j]=='1' && !vis[cur_i+1][cur_j]){
-                            dfs.push({cur_i+1, cur_j}) ;
-                            //vis[cur_i+1][cur_j]=true;
-                        }
-                        if(cur_i-1 >=0 && grid[cur_i-1][cur_j] =='1' && !vis[cur_i-1][cur_j]){
-                            dfs.push({cur_i-1, cur_j}) ;
-                            //vis[cur_i-1][j]=true;
-                        }
-                        if(cur_j+1 <m && grid[cur_i][cur_j+1]=='1' && !vis[cur_i][cur_j+1]){
-                            dfs.push({cur_i, cur_j+1}) ; 
-                            //vis[cur_i][cur_j+1]=true;
-                        }
-                        if(cur_j-1 >=0 && grid[cur_i][cur_j-1]=='1' && !vis[cur_i][cur_j-1]) {
-                            dfs.push({cur_i, cur_j-1}) ; 
-                            //vis[cur_i][cur_j-1]=true;
-                        }
-                    }
+                    dfs(grid,m,{i,j});
                 }
             }
         }
-        return ans; 
+        
+        return ans;
+            
     }
 };
